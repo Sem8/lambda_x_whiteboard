a49c0c94c0ff95e5c0a8c2c230360e16b1f6698d
Sem8 <mssemmi8@gmail.com>
2019-09-06 14:56:41 -0400

    3rd solution to longest common prefix problem still with O(n^2) time complexity

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -18,20 +18,47 @@
 // };
 
 // 2nd solution O(n^2):
+// const longestCommonPrefix = (strArr) => {
+//     let longestPrefix = '';
+//     if (strArr.length > 0) {
+//         longestPrefix = strArr[0];
+//     }
+//     for (let i = 0; i < strArr.length; i++) {
+//         for (let j = 0; j < longestPrefix.length; j++) {
+//             if (strArr[i][j] != longestPrefix[j]) {
+//                 longestPrefix = longestPrefix.slice(0, j);
+//             }
+//         }
+//     }
+//     return longestPrefix;
+// }
+
+// 3rd solution O(n^2):
 const longestCommonPrefix = (strArr) => {
+    if (strArr.length === 0) return '';
+    if (strArr.length === 1) return strArr[0];
+
     let longestPrefix = '';
-    if (strArr.length > 0) {
-        longestPrefix = strArr[0];
-    }
-    for (let i = 0; i < strArr.length; i++) {
-        for (let j = 0; j < longestPrefix.length; j++) {
-            if (strArr[i][j] != longestPrefix[j]) {
-                longestPrefix = longestPrefix.slice(0, j);
-            }
+    let pointer = 0;
+    let currentChar = '';
+    let stillSearching = true;
+
+    while (stillSearching) {
+        currentChar = strArr[0][pointer];
+
+        for (let i = 1; i < strArr.length; i++) {
+            if (strArr[i][pointer] !== currentChar) stillSearching = false;
+        }
+
+        if (stillSearching) {
+            longestPrefix += currentChar;
+            pointer++;
         }
     }
     return longestPrefix;
+
 }
+
 
 console.log(longestCommonPrefix(["internetly", "internet", "internets"]));
 console.log(longestCommonPrefix(['a', 'b', 'c', 'd']));
