7a538400ab655f5db4e8d0fae26c4e164f2fbd00
Sem8 <mssemmi8@gmail.com>
2019-12-13 20:44:21 -0500

    wrote out my own solution code and pseudocode for huffman decoding of a string by binary tree but did not pass all tests and I do not know why even though logic is similar to suggested solution

---
+++ huffmanDecoding.py
@@ -0,0 +1,84 @@
+import queue as Queue
+
+cntr = 0
+
+class Node:
+    def __init__(self, freq, data):
+        self.freq = freq
+        self.data = data
+        self.left = None
+        self.right = None
+        global cntr
+        self._count = cntr
+        cntr = cntr + 1
+        
+    def __lt__(self, other):
+        if self.freq != other.freq:
+            return self.freq < other.freq
+        return self._count < other._count
+
+def huffman_hidden():#builds the tree and returns root
+    q = Queue.PriorityQueue()
+
+    
+    # for key in freq:
+    #     q.put((freq[key], key, Node(freq[key], key) ))
+    
+    while q.qsize() != 1:
+        a = q.get()
+        b = q.get()
+        obj = Node(a[0] + b[0], '\0' )
+        obj.left = a[2]
+        obj.right = b[2]
+        q.put((obj.freq, obj.data, obj ))
+        
+    root = q.get()
+    root = root[2] #contains root object
+    return root
+
+def dfs_hidden(obj, already):
+    if(obj == None):
+        return
+    # elif(obj.data != '\0'):
+    #     code_hidden[obj.data] = already
+        
+    dfs_hidden(obj.right, already + "1")
+    dfs_hidden(obj.left, already + "0")
+
+# My solution: Did not pass all tests
+''' Pseudocode:
+1. Declare a variable called current and set it equal to root to serve as a pointer to root 
+2. Declare a variable called idx and set it equal to 0 (it'll serve as an index for the s)
+3. Initialize an empty string, call it finalStr
+4. Make a while loop of while idx is less than length of s
+5. Inside while loop make an if statement of s at index idx is 1 then go right by setting current to current.right then increment idx by 1
+6. Inside previous if statement, make another if statement of if current.data exists (or is a valid value) then add current.data to finalStr
+and reset current to equal to root. 
+7. Inside still that 1st if statement, make an else statement (if current.data does not exist) then make a while loop of while current.data
+does not exist
+8. Inside inner while loop, make an if statement of if value of s at index idx is 1 then set current to current.right, and increment idx by 1
+and if value of s at index idx is 0 then set current to current.left and increment idx by 1
+'''
+
+def decodeHuff(root, s):
+    current = root
+    idx = 0
+    finalStr = ''
+
+    while idx < len(s):
+        if s[idx] == '1':
+            current = current.right
+            idx += 1
+
+            if (current.left is None) and (current.right is None):
+                finalStr += current.data
+                current = root
+            
+        if s[idx] == '0':       
+            current = current.left
+            idx += 1
+            if (current.left is None) and (current.right is None):
+                finalStr += current.data
+                current = root
+            
+    return finalStr
--- lowestCommonAncestor.py
+++ lowestCommonAncestor.py
@@ -98,7 +98,22 @@ def lca(root, v1, v2):
         # else:
         #     return root
     return root
-  
+
+# Suggested solution:
+# def lca(root, v1, v2):
+#     if v1 < root.info and v2 < root.info:
+#         # traverse to the left 
+#         if root.left:
+#             return lca(root.left, v1, v2) 
+#     elif v1 > root.info and v2 > root.info:
+#         # traverse to the right 
+#         if root.right:
+#             return lca(root.right, v1, v2)
+#     # otherwise, v1 and v2 are on opposite sides and we've 
+#     # found the lca 
+#     return root
+
+
 a = BinarySearchTree()
 a.create(4)
 a.create(2)
